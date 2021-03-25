e5f826e0e2e2c9984f476b29473e5a1b7b948f5c
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-25 15:34:55 +0000

    change layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <Header />
+    <nav class="flex items-center justify-between flex-wrap bg-teal p-6">
+      <div class="flex items-center flex-no-shrink text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+      </div>
+      <div class="block lg:hidden">
+        <button
+          class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
+          @click="drawer = !drawer"
+        >
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div
+        class="w-full lg:visible block flex-grow lg:flex lg:items-center lg:w-auto"
+        :class="{ 'hidden': !drawer }"
+      >
+        <div class="text-sm lg:flex-grow">
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+            Docs
+          </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
+            Examples
+          </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
+            Blog
+          </a>
+        </div>
+        <div>
+          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Download</a>
+        </div>
+      </div>
+    </nav>
+
     <Nuxt />
     <Hooter />
   </div>
